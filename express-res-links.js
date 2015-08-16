b88680fdb87dfd4975dbd568a7ac253315354935
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-08-16 08:23:01 +0000

    Enforce arg0 being an object

--- express-res-links.js
+++ express-res-links.js
@@ -2,6 +2,10 @@ import {stringify as stringifyLinks} from 'http-header-link'
 
 export default function (req, res, next) {
   res.links = function links (linkObj, lang = false) {
+    if (typeof linkObj !== 'object') {
+      throw new Error('res.links expects linkObj to be an object')
+    }
+
     let val = this.get('Link') || ''
     lang = lang || this.get('Content-Language') || linkObj.defaultLang
 
